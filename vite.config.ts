f64342740b427cd8d08f003d9cd82438cb34ecde
Vital Rusinov <Vitaliy.rusinov90@gmail.com>
2024-12-25 17:41:12 +0500

    update vite.config.ts, tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Quiz/',
+  resolve: {
+      alias: {
+          app: '/src/app',
+          entities: '/src/entities',
+          features: '/src/features',
+          pages: '/src/pages',
+          shared: '/src/shared',
+          widgets: '/src/widgets',
+      },
+  },
 })
